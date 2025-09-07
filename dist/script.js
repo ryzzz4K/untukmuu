e6e9096289f1685a4f2216cceaf2ba13bfbf9231
ryz4K <najisebew@gmail.com>
2025-09-07 14:12:19 +0700

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -289,7 +289,7 @@ class World {
       this.sound = new THREE.Audio(listener);
       const audioLoader = new THREE.AudioLoader();
       audioLoader.load(
-      "https://assets.codepen.io/74321/ukulele.mp3",
+      "7444129453820644112 (1).mp3",
       buffer => {
         this.sound.setBuffer(buffer);
         this.sound.setLoop(false);
